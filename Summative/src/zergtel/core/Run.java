da3706fad3daef46f45b3450cd1a830166854f92
blazequilava <heroicwaffles@gmail.com>
2016-09-15 13:01:12 -0400

    This is so true, he is the sailtiest of salty memes

--- Summative/src/zergtel/core/Run.java
+++ Summative/src/zergtel/core/Run.java
@@ -2,8 +2,9 @@ package zergtel.core;
 
 public class Run {
 
+	public static String michaelZhu = "salty meme";
 	public static void main(String[] args) {
-		while (0 != 1){
+		while (!michaelZhu.contains("not salty meme")){
 		System.out.println("Michael Zhu is a salty meme ");
 		}
 	}
