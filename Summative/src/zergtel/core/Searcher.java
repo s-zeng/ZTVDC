0472d619645ea8a113d84b06a676470695201a2c
blazequilava <heroicwaffles@gmail.com>
2016-09-19 13:23:25 -0400

    owoluthqowehfiwuefhiweh;iuqwerh;oierhe;ofheo;fheorfwefeiurgfbeiurgbe;orugbeourbferofbqoerfbqoe;rfbouerf;ouerfboe;urbfo;uergboue;ngqoe;rbouqerbgoequrboueqbfowoluthqowehfiwuefhiweh;iuqwerh;oierhe;ofheo;fheorfwefeiurgfbeiurgbe;orugbeourbferofbqoerfbqoe;rfbouerf;ouerfboe;urbfo;uergboue;ngqoe;rbouqerbgoequrboueqbfowoluthqowehfiwuefhiweh;iuqwerh;oierhe;ofheo;fheorfwefeiurgfbeiurgbe;orugbeourbferofbqoerfbqoe;rfbouerf;ouerfboe;urbfo;uergboue;ngqoe;rbouqerbgoequrboueqbfowoluthqowehfiwuefhiweh;iuqwerh;oierhe;ofheo;fheorfwefeiurgfbeiurgbe;orugbeourbferofbqoerfbqoe;rfbouerf;ouerfboe;urbfo;uergboue;ngqoe;rbouqerbgoequrboueqbfowoluthqowehfiwuefhiweh;iuqwerh;oierhe;ofheo;fheorfwefeiurgfbeiurgbe;orugbeourbferofbqoerfbqoe;rfbouerf;ouerfboe;urbfo;uergboue;ngqoe;rbouqerbgoequrboueqbfowoluthqowehfiwuefhiweh;iuqwerh;oierhe;ofheo;fheorfwefeiurgfbeiurgbe;orugbeourbferofbqoerfbqoe;rfbouerf;ouerfboe;urbfo;uergboue;ngqoe;rbouqerbgoequrboueqbfowoluthqowehfiwuefhiweh;iuqwerh;oierhe;ofheo;fheorfwefeiurgfbeiurgbe;orugbeourbferofbqoerfbqoe;rfbouerf;ouerfboe;urbfo;uergboue;ngqoe;rbouqerbgoequrboueqbfowoluthqowehfiwuefhiweh;iuqwerh;oierhe;ofheo;fheorfwefeiurgfbeiurgbe;orugbeourbferofbqoerfbqoe;rfbouerf;ouerfboe;urbfo;uergboue;ngqoe;rbouqerbgoequrboueqbfowoluthqowehfiwuefhiweh;iuqwerh;oierhe;ofheo;fheorfwefeiurgfbeiurgbe;orugbeourbferofbqoerfbqoe;rfbouerf;ouerfboe;urbfo;uergboue;ngqoe;rbouqerbgoequrboueqbfowoluthqowehfiwuefhiweh;iuqwerh;oierhe;ofheo;fheorfwefeiurgfbeiurgbe;orugbeourbferofbqoerfbqoe;rfbouerf;ouerfboe;urbfo;uergboue;ngqoe;rbouqerbgoequrboueqbfowoluthqowehfiwuefhiweh;iuqwerh;oierhe;ofheo;fheorfwefeiurgfbeiurgbe;orugbeourbferofbqoerfbqoe;rfbouerf;ouerfboe;urbfo;uergboue;ngqoe;rbouqerbgoequrboueqbfowoluthqowehfiwuefhiweh;iuqwerh;oierhe;ofheo;fheorfwefeiurgfbeiurgbe;orugbeourbferofbqoerfbqoe;rfbouerf;ouerfboe;urbfo;uergboue;ngqoe;rbouqerbgoequrboueqbfowoluthqowehfiwuefhiweh;iuqwerh;oierhe;ofheo;fheorfwefeiurgfbeiurgbe;orugbeourbferofbqoerfbqoe;rfbouerf;ouerfboe;urbfo;uergboue;ngqoe;rbouqerbgoequrboueqbfowoluthqowehfiwuefhiweh;iuqwerh;oierhe;ofheo;fheorfwefeiurgfbeiurgbe;orugbeourbferofbqoerfbqoe;rfbouerf;ouerfboe;urbfo;uergboue;ngqoe;rbouqerbgoequrboueqbfowoluthqowehfiwuefhiweh;iuqwerh;oierhe;ofheo;fheorfwefeiurgfbeiurgbe;orugbeourbferofbqoerfbqoe;rfbouerf;ouerfboe;urbfo;uergboue;ngqoe;rbouqerbgoequrboueqbfowoluthqowehfiwuefhiweh;iuqwerh;oierhe;ofheo;fheorfwefeiurgfbeiurgbe;orugbeourbferofbqoerfbqoe;rfbouerf;ouerfboe;urbfo;uergboue;ngqoe;rbouqerbgoequrboueqbfowoluthqowehfiwuefhiweh;iuqwerh;oierhe;ofheo;fheorfwefeiurgfbeiurgbe;orugbeourbferofbqoerfbqoe;rfbouerf;ouerfboe;urbfo;uergboue;ngqoe;rbouqerbgoequrboueqbfowoluthqowehfiwuefhiweh;iuqwerh;oierhe;ofheo;fheorfwefeiurgfbeiurgbe;orugbeourbferofbqoerfbqoe;rfbouerf;ouerfboe;urbfo;uergboue;ngqoe;rbouqerbgoequrboueqbfowoluthqowehfiwuefhiweh;iuqwerh;oierhe;ofheo;fheorfwefeiurgfbeiurgbe;orugbeourbferofbqoerfbqoe;rfbouerf;ouerfboe;urbfo;uergboue;ngqoe;rbouqerbgoequrboueqbfowoluthqowehfiwuefhiweh;iuqwerh;oierhe;ofheo;fheorfwefeiurgfbeiurgbe;orugbeourbferofbqoerfbqoe;rfbouerf;ouerfboe;urbfo;uergboue;ngqoe;rbouqerbgoequrboueqbfowoluthqowehfiwuefhiweh;iuqwerh;oierhe;ofheo;fheorfwefeiurgfbeiurgbe;orugbeourbferofbqoerfbqoe;rfbouerf;ouerfboe;urbfo;uergboue;ngqoe;rbouqerbgoequrboueqbfowoluthqowehfiwuefhiweh;iuqwerh;oierhe;ofheo;fheorfwefeiurgfbeiurgbe;orugbeourbferofbqoerfbqoe;rfbouerf;ouerfboe;urbfo;uergboue;ngqoe;rbouqerbgoequrboueqbfowoluthqowehfiwuefhiweh;iuqwerh;oierhe;ofheo;fheorfwefeiurgfbeiurgbe;orugbeourbferofbqoerfbqoe;rfbouerf;ouerfboe;urbfo;uergboue;ngqoe;rbouqerbgoequrboueqbf

--- Summative/src/zergtel/core/Searcher.java
+++ Summative/src/zergtel/core/Searcher.java
@@ -1,5 +1,5 @@
 package zergtel.core;
 
 public class Searcher {
-
+//this is a totally usefull comment owoluthqowehfiwuefhiweh;iuqwerh;oierhe;ofheo;fheorfwefeiurgfbeiurgbe;orugbeourbferofbqoerfbqoe;rfbouerf;ouerfboe;urbfo;uergboue;ngqoe;rbouqerbgoequrboueqbf
 }
