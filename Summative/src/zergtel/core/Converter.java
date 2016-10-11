152e492ac22d2cc7c6fd073bc62ca7dc18a5ceba
blazequilava <heroicwaffles@gmail.com>
2016-10-11 13:15:04 -0400

    Potential library use

--- Summative/src/zergtel/core/Converter.java
+++ Summative/src/zergtel/core/Converter.java
@@ -2,7 +2,7 @@ package zergtel.core;
 
 public class Converter {
 	//Test commit
-
+	//JAVE Library http://www.sauronsoftware.it/projects/jave/index.php
 	//Potential libraries: Joba Time, Java Media Framework API (JMF)
 
 }
