784d29bd8304614d1ea897e4dc81785f3b0e5aad
blazequilava <heroicwaffles@gmail.com>
2016-10-18 14:10:14 -0400

    another link

--- Summative/src/zergtel/core/Downloader.java
+++ Summative/src/zergtel/core/Downloader.java
@@ -4,4 +4,5 @@ public class Downloader {
 	//Some inspirational code:
 	//https://sourceforge.net/p/ytd2/code/HEAD/tree/trunk/ytd2/src/main/java/zsk/YTDownloadThread.java 
 	//https://github.com/ytsdk/ytsdk
+	//http://stackoverflow.com/questions/4032766/how-to-download-videos-from-youtube-on-java
 }
