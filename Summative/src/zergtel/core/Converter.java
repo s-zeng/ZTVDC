dbdaf01dbc462137bbd0fced9b82b496312bfd9b
blazequilava <heroicwaffles@gmail.com>
2016-10-18 14:16:05 -0400

    Converter Example

--- Summative/src/zergtel/core/Converter.java
+++ Summative/src/zergtel/core/Converter.java
@@ -7,5 +7,5 @@ public class Converter {
 	//https://docs.oracle.com/javase/tutorial/sound/converters.html
 	//JAVE Library http://www.sauronsoftware.it/projects/jave/index.php
 	//Stuff to experiment with: Joba Time, Java Media Framework API (JMF)
-
+	//https://github.com/Rebant/Video-Converter/blob/master/videoConverter.java
 }
