b63bfd6b6f8dd734b125ec894c18c2f9d8be0ecc
kronicmage <kronicmage@gmail.com>
2016-10-20 14:06:29 -0400

    Refactoring

--- Summative/src/zergtel/core/downloader/Http.java
+++ Summative/src/zergtel/core/downloader/Http.java
@@ -5,5 +5,12 @@ import java.net.*;
 
 
 public class Http {
-
+	public static File get(String uri) throws Exception {
+		return get(new URL(uri));
+	}
+	
+	public static File get(URL uri) throws Exception {
+		File x = null;
+		return x;
+	}
 }
