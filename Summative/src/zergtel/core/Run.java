fe9b6da2708fbf5922ce7a011baa4f063f0209f3
kronicmage <kronicmage@gmail.com>
2016-10-20 14:08:26 -0400

    Run class test

--- Summative/src/zergtel/core/Run.java
+++ Summative/src/zergtel/core/Run.java
@@ -1,11 +1,15 @@
 package zergtel.core;
 
+import zergtel.core.downloader.Http;
+
 public class Run {
 
 	public static String michaelZhu = "salty meme";
 	public static void main(String[] args) {
-		while (!michaelZhu.contains("not salty meme")){
-		System.out.println("Michael Zhu is a salty meme ");
+		try {
+			Http.get("test");
+		} catch (Exception e) {
+			
 		}
 	}
 
