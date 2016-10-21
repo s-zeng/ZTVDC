4a45da3e58c4a557253aadb72b00d1b568596fa0
kronicmage <kronicmage@gmail.com>
2016-10-21 12:57:04 -0400

    more intellij stuff

--- Summative/src/zergtel/core/Run.java
+++ Summative/src/zergtel/core/Run.java
@@ -4,12 +4,11 @@ import zergtel.core.downloader.Http;
 
 public class Run {
 
-	public static String michaelZhu = "salty meme";
 	public static void main(String[] args) {
 		try {
 			Http.get("test");
 		} catch (Exception e) {
-			
+			e.printStackTrace();
 		}
 	}
 
--- Summative/src/zergtel/core/downloader/Http.java
+++ Summative/src/zergtel/core/downloader/Http.java
@@ -5,12 +5,28 @@ import java.net.*;
 
 
 public class Http {
-	public static File get(String uri) throws Exception {
+	public static String get(String uri) throws Exception {
 		return get(new URL(uri));
 	}
-	
-	public static File get(URL uri) throws Exception {
-		File x = null;
-		return x;
+
+	public static String get(URL uri) throws Exception {
+		URLConnection connection = uri.openConnection();
+		BufferedReader in = new BufferedReader(
+				new InputStreamReader(
+						connection.getInputStream()));
+
+		FileWriter writer = new FileWriter(uri.getFile());
+
+		String lineInput;
+		while ((lineInput = in.readLine()) != null) {
+			writer.write(lineInput);
+
+		}
+
+
+		in.close();
+		writer.close();
+
+		return "";
 	}
 }
