7b0a3ae168d5b649d5fb2e60cf074dfaacb63c91
kronicmage <kronicmage@gmail.com>
2016-10-24 14:47:48 -0400

    Implemented new threadsafe http class

--- Summative/src/zergtel/core/downloader/Downloader.java
+++ Summative/src/zergtel/core/downloader/Downloader.java
@@ -5,6 +5,7 @@ public class Downloader {
 	//https://sourceforge.net/p/ytd2/code/HEAD/tree/trunk/ytd2/src/main/java/zsk/YTDownloadThread.java 
 	//https://github.com/ytsdk/ytsdk
 	//http://stackoverflow.com/questions/4032766/how-to-download-videos-from-youtube-on-java
+    //http://superuser.com/questions/773719/how-do-all-of-these-save-video-from-youtube-services-work?noredirect=1&lq=1
 
     /**To download from bandcamp:
      * In the script sections of the html of any album/track page, ther will be a JSON object in the form of var TralbumData -
---
+++ Summative/src/zergtel/core/downloader/HttpOld.java
@@ -0,0 +1,69 @@
+package zergtel.core.downloader;
+
+import java.io.*;
+import java.net.*;
+
+
+public class HttpOld {
+	private static final int BUFFER_SIZE = 4096;
+	private static final String DOWNLOAD_LOCATION = "download/";
+
+//	Todo - method to avoid naming conflicts (e.g. save as example(1).mp3 if example.mp3 already exists)
+//		maybe add seperate class for file things like this and cleanseName
+
+
+	public static String get(String uri) throws Exception {
+		return get(new URL(uri), "");
+	}
+
+	public static String get(String uri, String fileName) throws Exception {
+		return get(new URL(uri), fileName);
+	}
+
+	private static String get(URL uri, String fileName) throws Exception {
+		//Merits some refactoring for efficiency this code does
+
+		if (fileName.equals("")) {
+			fileName = getFileOutputName(uri);
+			System.out.println("File name: " + fileName);
+		}
+
+		fileName = DOWNLOAD_LOCATION.concat(cleanseName(fileName));
+
+		URLConnection connection = uri.openConnection();
+		int size = connection.getContentLength();
+
+		InputStream in = connection.getInputStream();
+		byte[] buffer = new byte[BUFFER_SIZE];
+		int n;
+		int downloaded = 0;
+
+
+		OutputStream out = new FileOutputStream(fileName);
+		while ((n = in.read(buffer)) != -1) {
+			out.write(buffer, 0, n);
+			downloaded += n;
+			System.out.println(Math.round((double)100*downloaded/size));
+		}
+		in.close();
+		out.close();
+
+		return fileName;
+	}
+
+	private static String getFileOutputName(URL uri) {
+		String output;
+		if (!uri.getFile().isEmpty()) {
+			output = uri.getFile();
+		} else {
+			output = uri.getHost() + uri.getPath();
+		}
+
+		return output;
+	}
+
+	private static String cleanseName(String fileName) {
+		//flesh this function out to be able to fix all illegal windows file names
+		return fileName.replaceAll("/", "");
+	}
+}
