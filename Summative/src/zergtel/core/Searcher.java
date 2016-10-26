dab56cda77b56f7543468617e88ff2ff95df6428
kronicmage <kronicmage@gmail.com>
2016-10-26 14:23:00 -0400

    Added maven support

--- Summative/src/zergtel/core/Searcher.java
+++
@@ -1,5 +0,0 @@
-package zergtel.core;
-public class Searcher {
-//this is a totally usefull comment owoluthqowehfiwuefhiweh;iuqwerh;oierhe;ofheo;fheorfwefeiurgfbeiurgbe;orugbeourbferofbqoerfbqoe;rfbouerf;ouerfboe;urbfo;uergboue;ngqoe;rbouqerbgoequrboueqbf
-//an actual usefull comment https://developers.google.com/youtube/v3/code_samples/java#search_by_keyword 
-}
