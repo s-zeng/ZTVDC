10e0704aab491772aef264f249dffae03d640bcb
blazequilava <heroicwaffles@gmail.com>
2016-10-26 14:18:26 -0400

    r

--- Summative/src/zergtel/core/converter/Converter.java
+++ Summative/src/zergtel/core/converter/Converter.java
@@ -5,13 +5,14 @@ import java.io.*;
 
 public class Converter 
 {
+    Process app;
     public void convert(File f, String n, String f1) //f1 = format
     {
         try {
             String outFile = n;
             String  format = f1;
             Runtime convert = Runtime.getRuntime();
-            final Process app = convert.exec("ffmpeg -i " + f.getName() + " " + outFile + format);
+            app = convert.exec("ffmpeg -i " + f.getName() + " " + outFile + format);
             BufferedReader appReader = new BufferedReader(new InputStreamReader(app.getInputStream()));
 
         } catch (IOException e) {
@@ -21,8 +22,14 @@ public class Converter
     }
 
     public void cancel()
-    {
-
+        {
+        OutputStream cancel = app.getOutputStream();
+            try {
+        cancel.write("q\n".getBytes());
+                cancel.flush();
+            } catch (IOException e) {
+                e.printStackTrace();
+        }
     }
     //http://stackoverflow.com/questions/17123118/how-to-stop-ffmpeg-that-runs-through-java-process
     //http://stackoverflow.com/questions/10927718/how-to-read-ffmpeg-response-from-java-and-use-it-to-create-a-progress-bar
