7aca560f6eafc475ab417370385b6cd895488a2e
kronicmage <kronicmage@gmail.com>
2016-10-26 14:42:32 -0400

    Updated maven dependencies to include VGet

--- Summative/src/main/java/zergtel/core/Run.java
+++ Summative/src/main/java/zergtel/core/Run.java
@@ -4,6 +4,8 @@ package zergtel.core;
 
 		import java.net.URL;
 
+import com.github.axet.vget.*;
+
 public class Run {
 
 	public static void main(String[] args) {
@@ -11,6 +13,7 @@ public class Run {
 //			HttpOld.get("http://popplers5.bandcamp.com/download/track?enc=mp3-128&fsig=8b732af24df036790dce29b8bf65fde4&id=214214452&stream=1&ts=1476937747.0", "Nascency.mp3");
 //			HttpOld.get("http://simonzeng.tk/example.mp3");
 			Http getter = new Http(new URL("http://simonzeng.tk/example.mp3"));
+
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
