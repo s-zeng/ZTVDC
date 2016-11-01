079698ba9502bcea4c320e33057c3cbe1b8fd603
blazequilava <heroicwaffles@gmail.com>
2016-11-01 13:23:41 -0400

    r

--- Summative/src/main/java/zergtel/core/io/Metadata.java
+++ Summative/src/main/java/zergtel/core/io/Metadata.java
@@ -14,4 +14,4 @@ public class Metadata {
 }
 
 //http://stackoverflow.com/questions/28082995/how-to-get-id3tags-information-of-a-audio-file-in-a-specific-url
-//https://github.com/sannies/mp4parser
+//https://github.com/sannies/mp4parser ... http://www.xuggle.com/downloads potential maven libraries to be added (don't add them until we meet up again Nov. 2 please
