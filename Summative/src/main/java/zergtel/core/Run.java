7386547f114e8f3be5f93e4151216826eda4416b
blazequilava <heroicwaffles@gmail.com>
2016-11-06 18:30:33 -0500

    I simplified metadata, changed the class to 1 method that gets metadata and adds it to the new file.

--- Summative/src/main/java/zergtel/core/Run.java
+++ Summative/src/main/java/zergtel/core/Run.java
@@ -13,9 +13,10 @@ public class Run {
 
 	public static void main(String[] args) {
 		File file = new File("C:/Users/User/IdeaProjects/summative/Summative/bin/file.mp4");
+		File file2 = new File("C:/Users/User/IdeaProjects/summative/Summative/bin/test.mp4");
 		BasicConfigurator.configure();
 		try{
-			Metadata.get(file);
+			Metadata.transfer(file, file2);
 		} catch (IOException e) {
 			System.err.println("doh I missed");
 		}
--- Summative/src/main/java/zergtel/core/io/Metadata.java
+++ Summative/src/main/java/zergtel/core/io/Metadata.java
@@ -1,49 +1,19 @@
 package zergtel.core.io;
 
-import com.xuggle.xuggler.ICodec;
-import com.xuggle.xuggler.IContainer;
-import com.xuggle.xuggler.IStream;
-import com.xuggle.xuggler.IStreamCoder;
+import com.xuggle.xuggler.*;
+
 import java.io.*;
 
 public class Metadata {
 
-    public static void get(File f) throws IOException
-    {
-        IContainer iC = IContainer.make();
-        int data = iC.open(f.getPath(), IContainer.Type.READ, null);
-        if (data < 0)
-            throw new RuntimeException("ripperoni with a side of spaghetti");
-        int numStreams = iC.getNumStreams();
-        long duration = iC.getDuration();
-        long fileSize = iC.getFileSize();
-        long bitRate = iC.getBitRate();
-        System.out.println(numStreams + "\n" + duration + "\n" + fileSize + "\n" + bitRate + "\n");
-        for(int i = 0; i < numStreams; i++)
-        {
-            IStream s = iC.getStream(i);
-            IStreamCoder iCS = s.getStreamCoder();
-            System.out.printf("%d", i);
-            System.out.printf("%s ", iCS.getCodecType());
-            System.out.printf("%s ", iCS.getCodecID());
-            System.out.printf("%s ", s.getDuration());
-            System.out.printf("%s", iC.getStartTime());
-            System.out.printf("%d%d", iCS.getTimeBase().getNumerator(), iCS.getTimeBase().getDenominator());
-            System.out.println("Audio/Video codec below");
-            if(iCS.getCodecType() == ICodec.Type.CODEC_TYPE_AUDIO)
-            {
-                System.out.printf("%d", iCS.getSampleRate());
-                System.out.printf("%d", iCS.getChannels());
-                System.out.printf("s", iCS.getSampleFormat());
-            }
-            else if (iCS.getCodecType() == ICodec.Type.CODEC_TYPE_VIDEO)
-            {
-                System.out.printf("%d", iCS.getWidth());
-                System.out.printf("%d", iCS.getHeight());
-                System.out.printf("%s", iCS.getPixelType());
-                System.out.printf("%5.2f", iCS.getFrameRate().getDouble());
-            }
-        }
+    public static void transfer(File f, File f2) throws IOException {
+        IContainer iR = IContainer.make();
+        int data = iR.open(f.getPath(), IContainer.Type.READ, null);
+        IMetaData metaData = iR.getMetaData();
+        IContainer iW = IContainer.make();
+        int data2 = iW.open(f2.getPath(), IContainer.Type.WRITE, null);
+        iW.setMetaData(metaData);
+
     }
 
 }
