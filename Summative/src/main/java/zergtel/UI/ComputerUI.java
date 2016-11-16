799a7ae9389ac7fee445fb06a2509677ba83a504
blazequilava <heroicwaffles@gmail.com>
2016-11-16 14:48:49 -0500

    r

--- Summative/src/main/java/zergtel/UI/ComputerUI.java
+++ Summative/src/main/java/zergtel/UI/ComputerUI.java
@@ -1,7 +1,22 @@
 package zergtel.UI;
 
+import javax.swing.*;
+import java.awt.*;
+
 /**
- * Created by User on 2016-10-25.
+ * Created by Shyam on 2016-10-25.
  */
-public class ComputerUI {
+public class ComputerUI extends JFrame {
+    private JButton download = new JButton();
+    private JButton convert = new JButton();
+    public ComputerUI() {
+        setExtendedState(MAXIMIZED_BOTH);
+        setTitle("ZergTel VDC");
+        setLayout(null);
+        setResizable(true);
+        Container c = getContentPane();
+        c.setBackground(new Color(44, 42, 43));
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setVisible(true);
+    }
 }
--- Summative/src/main/java/zergtel/core/Run.java
+++ Summative/src/main/java/zergtel/core/Run.java
@@ -1,6 +1,7 @@
 package zergtel.core;
 
 import org.apache.log4j.BasicConfigurator;
+import zergtel.UI.ComputerUI;
 import zergtel.core.converter.Converter;
 import zergtel.core.io.Metadata;
 import org.slf4j.LoggerFactory;
@@ -11,9 +12,6 @@ import java.io.*;
 public class Run {
 
 	public static void main(String[] args) {
-		File file = new File("C:/Users/User/IdeaProjects/summative/Summative/bin/file.mp4");
-		File f2 = new File("C:/Users/User/IdeaProjects/summative/Summative/bin/hi.mp3");
-		Converter c = new Converter();
-		c.merge(file, f2, "test4", "mp4");
+		ComputerUI ui = new ComputerUI();
 				}
 }
