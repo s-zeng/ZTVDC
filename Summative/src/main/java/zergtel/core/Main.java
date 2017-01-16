ed0b0e7143429f0656ff4a9f2bcfb91348c25d13
kronicmage <kronicmage@gmail.com>
2017-01-15 21:16:22 -0500

    Temp directory clearing

--- Summative/src/main/java/zergtel/core/Main.java
+++ Summative/src/main/java/zergtel/core/Main.java
@@ -2,10 +2,22 @@ package zergtel.core;
 
 
 import zergtel.UI.ComputerUI;
+import zergtel.core.downloader.EzHttp;
+
+import java.io.File;
 
 public class Main {
 
 	public static void main(String[] args) {
 		ComputerUI ui = new ComputerUI();
+		Runtime.getRuntime().addShutdownHook(new Thread() {
+			@Override
+			public void run() {
+				File temp = new File(EzHttp.TEMP_LOCATION);
+				for (File file : temp.listFiles()) {
+					file.delete();
+				}
+			}
+		});
 	}
 }
