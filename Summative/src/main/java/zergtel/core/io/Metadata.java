3ee581a4103c94cb54644b96b3b032b4b2af4b63
Shyam <heroicwaffles@gmail.com>
2017-01-22 12:21:53 -0500

    Commented Depricated Metadata class

--- Summative/src/main/java/zergtel/core/io/Metadata.java
+++ Summative/src/main/java/zergtel/core/io/Metadata.java
@@ -9,17 +9,20 @@ import java.util.Map;
 
 public class Metadata {
 
+    /**
+     * Sets the metadata of a file
+     * @param file the file that will have the information stored into
+     * @param map Information of the Metadata to be added to the file
+     * This class was depricated
+     */
     public static void set(File file, Map<String, String> map)
     {
-        IContainer iW = IContainer.make();
-        iW.open(file.getPath(), IContainer.Type.WRITE, null);
-        IMetaData data = IMetaData.make();
-        //data.setValue(); // setValue(String keys, String value)
-        iW.setMetaData(data);
-        iW.close();
+        IContainer iW = IContainer.make(); //creates an IContainer from xuggle libraries
+        iW.open(file.getPath(), IContainer.Type.WRITE, null); //sets the IContainer to write information inside of the file received.
+        IMetaData data = IMetaData.make(); //Makes the Metadata
+        //data.setValue(); // setValue(String keys, String value) //Sets the metadata value received from the map
+        iW.setMetaData(data); //sets the metadata into the file opened
+        iW.close(); //closes the IContainer
     }
 
 }
-
-//http://stackoverflow.com/questions/28082995/how-to-get-id3tags-information-of-a-audio-file-in-a-specific-url
-//https://github.com/sannies/mp4parser ... http://www.xuggle.com/downloads potential maven libraries to be added (don't add them until we meet up again Nov. 2 please
