cc7272f0a3e6f447695c162d30641a5f1cb669fd
kronicmage <kronicmage@gmail.com>
2017-01-22 14:36:16 -0500

    Documented Searcher

--- Summative/src/main/java/zergtel/core/searcher/Searcher.java
+++ Summative/src/main/java/zergtel/core/searcher/Searcher.java
@@ -17,6 +17,10 @@ import java.util.Properties;
 /**
  * Created by Simon on 1/9/2017.
  * Based largely off of Google's searcher example class
+ *
+ * You must have a valid youtube api key in java/resources/youtube.properties in order for searching to work; see
+ * https://developers.google.com/youtube/v3/getting-started for instructions on how to acquire one
+ *
  * This file contains some structures designed to include support for pages of searching,
  * but that feature hasn't been implemented yet as of 2017-01-10
  *
@@ -26,16 +30,47 @@ public class Searcher {
     private static final String PROPERTIES_FILENAME = "youtube.properties";
     private static YouTube youtube;
 
+    /**
+     * Searches youtube for a given query
+     *
+     * @param query - Search query
+     * @return An ArrayList of String-String maps, where each map is a single search result, and each map contains the
+     * following keys:
+     *   - title: contains title of video
+     *   - url: contains embed url of video
+     *   - thumbnail: contains url to standard resolution thumbnail of video
+     *   - description: provides a truncated (by google, don't know the exact length) description
+     *   - channel: name of the channel
+     *   - datePublished: returns date published in ISO format without time (YYYY-MM-DD)
+     */
     public static ArrayList<Map<String, String>> search(String query) {
         Properties properties = getProperties(PROPERTIES_FILENAME);
         return search(query, 0, Integer.parseInt(properties.getProperty("youtube.numreturns")), properties);
     }
 
+    /**
+     * Searches youtube for a given query (paged). Non-functional at the moment.
+     *
+     * @param query - Search query
+     * @param beginIndex - beginning index of search result (deprecated)
+     * @param endIndex - ending index of search result (deprecated)
+     * @return See search(String query)
+     */
     public static ArrayList<Map<String, String>> search(String query, int beginIndex, int endIndex) {
         return search(query, beginIndex, endIndex, getProperties(PROPERTIES_FILENAME));
     }
 
-    public static ArrayList<Map<String, String>> search(String query, int beginIndex, int endIndex, Properties properties) {
+    /**
+     * Same as the others, but this one allows for stateless passing of the properties, which means properties can be
+     * updated as the program is running
+     *
+     * @param query - See search(String query, int beginIndex, int endIndex)
+     * @param beginIndex - See search(String query, int beginIndex, int endIndex)
+     * @param endIndex - See search(String query, int beginIndex, int endIndex)
+     * @param properties - A properties object containing the youtube apikey
+     * @return - See search(String query)
+     */
+    private static ArrayList<Map<String, String>> search(String query, int beginIndex, int endIndex, Properties properties) {
         try {
             youtube = new YouTube.Builder(Auth.HTTP_TRANSPORT, Auth.JSON_FACTORY, request -> {
             }).setApplicationName("ZTVDC").build();
@@ -66,6 +101,12 @@ public class Searcher {
         return null;
     }
 
+    /**
+     * Gets properties from .properties files
+     *
+     * @param fileName - path and name of properties file
+     * @return A Properties object with all the properties found in the file
+     */
     private static Properties getProperties(String fileName) {
         Properties properties = new Properties();
         try {
