04c0a7fdd7e05a69d2cc41e6006e6256f883ed12
Shyam <heroicwaffles@gmail.com>
2017-01-22 12:07:58 -0500

    Commented Main class

--- Summative/src/main/java/zergtel/core/Main.java
+++ Summative/src/main/java/zergtel/core/Main.java
@@ -8,9 +8,11 @@ import java.io.File;
 
 public class Main {
 	public static ComputerUI ui;
-
+	//main method which runs our program (as seen by the (String[] args))
 	public static void main(String[] args) {
+		//runs ComputerUI
 		ui = new ComputerUI();
+		//clears temp folder when user closes JFrame
 		Runtime.getRuntime().addShutdownHook(new Thread() {
 			@Override
 			public void run() {
